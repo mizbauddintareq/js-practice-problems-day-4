77b4218f1a1f068620c695e947508f4a48575afb
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-09-15 01:06:16 +0600

    solved all problems

--- problems.js
+++ problems.js
@@ -1,12 +1,12 @@
-/* 1. Write a function that will take hour as the input parameter and will
-convert it into minutes and will return the result in minutes. 
+/*  1. Write a function that will take hour as the input parameter and will
+ convert it into minutes and will return the result in minutes.
 
 function hourToMin(hour) {
   const min = hour * 60;
   return min;
 }
 
-const myHour = hourToMin(2);
+const myHour = hourToMin(3);
 console.log(myHour); */
 
 /* 2. Write a function findLeapYear() that will take the array
@@ -24,7 +24,7 @@ function findLeapYear(year) {
   return leapYear;
 }
 
-const years = [2023, 2024, 2025, 2028, 2030];
+const years = [2023, 2024, 2012, 2016, 2017, 2025, 2028, 2030];
 console.log(findLeapYear(years)); */
 
 /* 3. Write a function findOddSum() that will take the array [5, 7, 8, 10, 45, 30]
@@ -42,6 +42,34 @@ function findOddSum(numbers) {
 
 const myNumbers = [5, 7, 8, 10, 45, 30];
 const allOddNumber = findOddSum(myNumbers);
-console.log(allOddNumber); */
+console.log(allOddNumber); 
 
-4. leapYear() নামে ফাংশন লিখো এবং নেক্সট ইয়ার অর্থাৎ ২০২৩ কি leap year নাকি সেটা চেক করো। Leap year হলে ফাংশন true রিটার্ন করবে আর leap year না হলে false রিটার্ন করবে।
+/* 4. leapYear() নামে ফাংশন লিখো এবং নেক্সট ইয়ার অর্থাৎ ২০২৩ কি leap year নাকি সেটা চেক করো। Leap year হলে ফাংশন true রিটার্ন করবে আর leap year না হলে false রিটার্ন করবে। 
+
+function leapYear(year) {
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(leapYear(2023)); 
+
+/* 5. তোমার বয়স কি odd নাকি even সংখ্যা সেটা চেক কর একটা ফাংশন দিয়ে। সেই ফাংশনকে কোন সংখ্যা প্যারামিটার হিসেবে দিলে, সেই সংখ্যা Even হলে ফাংশন true রিটার্ন করবে আর Odd হলে false রিটার্ন করবে।
+
+function odd_Even(number) {
+  if (number % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(odd_Even(29)); */
+
+/* 6. মনে করো তোমার ৪ ঘণ্টা লাগবে ঢাকাই পোঁছতে। তুমি এখন এ ভ্যালু ৪ কে একটা ফাংশনে পাঠাবা এবং সে ফাংশন ভেলু ৪ কে second এ convert করে তোমাকে রিটার্ন করবে।
+
+function hourToSec(hour) {
+  const sec = hour * 60 * 60;
+  return sec;
+}
+console.log(hourToSec(4)); */
